e68521903c8d5cf43094bccbb42d86c0b3dfdfaa
Ahmet Atakan <atakanahmetce@gmail.com>
2022-06-15 10:32:25 +0300

    benchmark test

---
+++ benchmark_offline.py
@@ -0,0 +1,55 @@
+import time
+from adhoccomputing.Experimentation.Topology import Topology
+from adhoccomputing.Networking.LogicalChannels.GenericChannel import GenericChannel
+from node_model.generic_node import GenericNode
+
+def main():
+  number_of_nodes = 4
+  topo = Topology()
+  topo.construct_winslab_topology_with_channels(number_of_nodes, GenericNode, GenericChannel)
+  topo.start()
+
+  """
+  shortest_paths = topo.allpairs_shortest_path()
+  for sp in shortest_paths:
+    print(shortest_paths[sp])
+    print('------------------------')
+  """
+
+  for i in range(5):
+    topo.nodes[0].send_message(1, "0#1")
+    topo.nodes[0].send_message(2, "0#2")
+    topo.nodes[0].send_message(3, "0#1")
+    topo.nodes[1].send_message(0, "1#0")
+    topo.nodes[1].send_message(2, "1#2")
+    topo.nodes[1].send_message(3, "1#3")
+    topo.nodes[2].send_message(0, "2#0")
+    topo.nodes[2].send_message(1, "2#1")
+    topo.nodes[2].send_message(3, "2#3")
+    topo.nodes[3].send_message(0, "3#0")
+    topo.nodes[3].send_message(1, "3#1")
+    topo.nodes[3].send_message(2, "3#2")
+    time.sleep(0.25)
+
+  
+
+  
+  print("######################################")
+  for node in topo.nodes.values():
+    print(node.application.identifier, ' stats')
+    print("**************************************")
+    print("received packets: ",node.application.received_packets)
+    print("sent     ack    : ",node.application.sent_ack)
+    print("--------------------------------------")
+    print("sent     packets: ",node.application.sent_packets)
+    print("received ack    : ",node.application.received_ack)
+    print("######################################")
+  
+  """  
+  for i in range(number_of_nodes):
+  neighbours = topo.get_neighbors(1)
+  print(neighbours)
+  """
+
+if __name__ == '__main__':
+  main()
---
+++ node_model/generic_node.py
@@ -0,0 +1,26 @@
+from adhoccomputing.GenericModel import GenericModel
+from adhoccomputing.Generics import ConnectorTypes, Event, EventTypes, GenericMessage, GenericMessageHeader
+from .app_layer import SAlohaAppLayer, SAlohaAppLayerEventTypes, SAlohaAppLayerMessageTypes
+
+class GenericNode(GenericModel):
+
+  def __init__(self, componentname, componentinstancenumber, context=None, configurationparameters=None, num_worker_threads=1, topology=None):
+    super().__init__(componentname, componentinstancenumber, context, configurationparameters, num_worker_threads, topology)
+    self.application = SAlohaAppLayer("SAlohaAppLayer", componentinstancenumber)
+    self.components.append(self.application)
+    self.application.connect_me_to_component(ConnectorTypes.DOWN, self)
+    self.connect_me_to_component(ConnectorTypes.UP, self.application)
+
+  def on_init(self, eventobj: Event):
+    pass
+
+  def on_message_from_top(self, eventobj: Event):
+    self.send_down(Event(self, EventTypes.MFRT, eventobj.eventcontent))
+
+  def on_message_from_bottom(self, eventobj: Event):
+    self.send_up(Event(self, EventTypes.MFRB, eventobj.eventcontent))
+
+  def send_message(self, dest_id, data):
+    header = GenericMessageHeader(SAlohaAppLayerMessageTypes.DATA, self.componentinstancenumber, dest_id)
+    message = GenericMessage(header, data)
+    self.application.send_self(Event(self, SAlohaAppLayerEventTypes.SENDDATA, message))
